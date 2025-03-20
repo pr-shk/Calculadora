9614b36e0200b98db3fbfedd5ab8eb80a9444d7f
Paulo Ventura <pr.shk+git@msn.com>
2025-03-20 17:13:10 -0300

    insere numero valido

--- calc.cpp
+++ calc.cpp
@@ -4,18 +4,47 @@ int main (void)
 {
     float var1, var2;
     char op, continuar;
+    bool valido;
 
     do
     {
+        valido = true;
         cout << " ENTRE COM A SUA EXPRESSAO (num operador num). Ex: 2 + 3 => ";
-        cin >> var1 >> op >> var2; /* le a expressao */
-        
-        // Validação do operador
-        while (op != '+' && op != '-' && op != '*' && op != '/') {
-            cout << "Operador invalido! Use apenas +, -, * ou /: ";
-            cin >> var1 >> op >> var2;
-        }
+        do{
+            // Validação do primeiro número
+            do {
+                valido = true;
+                cin >> var1;
+                if (cin.fail()) {
+                    cout << "Por favor, insira apenas numeros para o primeiro valor: ";
+                    cin.clear();
+                    cin.ignore(10000, '\n');
+                    valido = false;
+                }
+            } while (!valido);
 
+            // Validação do operador
+            do {
+                valido = true;
+                cin >> op;
+                if (op != '+' && op != '-' && op != '*' && op != '/') {
+                    cout << "Operador invalido! Use apenas +, -, * ou /: ";
+                    valido = false;
+                }
+            } while (!valido);
+
+            // Validação do segundo número
+            //do {
+                valido = true;
+                cin >> var2;
+                if (cin.fail()) {
+                    cout << "Por favor, insira apenas numeros para o segundo valor: ";
+                    cin.clear();
+                    cin.ignore(10000, '\n');
+                    valido = false;
+                }
+            //} while (!valido);
+        } while (!valido);
         switch (op)
             {
                 case '+': 
@@ -37,6 +66,11 @@ int main (void)
 
         cout << "\nDeseja fazer outra operacao? (s/n): ";
         cin >> continuar;
+        while (continuar != 's' && continuar != 'S' && continuar != 'n' && continuar != 'N')
+        {
+            cout << "Opcao invalida! Use apenas s ou n: ";
+            cin >> continuar;
+        }
     } while (continuar == 's' || continuar == 'S');
 
     cout << "\nPrograma encerrado. Obrigado por usar a calculadora!\n";
